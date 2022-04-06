653a4118ecf9b352beea7e5319cb929975fcc3d9
sushicat <alee@madisoncollege.edu>
2022-04-06 09:24:18 -0500

    create Play.js and Stats.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
+import { Play } from './Play';
+import { Stats } from './Stats';
+import { useState } from 'react';
+
 
 const App = ()=> {
   return (
     <div className="App">
-
+      <p>app</p>
     </div>
   );
 }
---
+++ src/Stats.js
@@ -0,0 +1,18 @@
+// import Button from '@mui/material/Button';
+import { useNavigate } from 'react-router-dom';
+
+export const Stats = () => {
+
+    const nav = useNavigate();
+
+    return (
+        <>
+            <h2>
+                Stats
+            </h2>
+
+
+
+        </>
+    );
+};
